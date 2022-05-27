c0724c6b9f9b0f2c85c3d273f3789eaaab1437eb
clarencelam <lam.clarence.bc@gmail.com>
2022-05-26 19:13:12 -0700

    add commenting functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Set the root route to "index" action of "articles" controller
   root "articles#index"
   # sets up conventional routes for articles resource
-  resources :articles
+  resources :articles do 
+    resources :comments 
+  end
 end
