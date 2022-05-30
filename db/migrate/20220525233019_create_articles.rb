20a2951f8451c2e13e92de725daa56993551eccd
clarencelam <lam.clarence.bc@gmail.com>
2022-05-30 14:26:39 -0700

    add subtitle

--- db/migrate/20220525233019_create_articles.rb
+++ db/migrate/20220525233019_create_articles.rb
@@ -2,6 +2,7 @@ class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
       t.string :title
+      t.string :subtitle
       t.text :body
 
       t.timestamps
